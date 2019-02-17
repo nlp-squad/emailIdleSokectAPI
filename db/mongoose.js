fb8ffbdff00381c16d09a5677f2c1a8d8f95338e
blessed-tawanda <blessedmahuni@gmail.com>
2019-02-17 21:47:22 +0200

    Added error handling on connecting to database

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const fs = require('fs')
 
 var options = {
   useNewUrlParser: true,
@@ -8,5 +9,11 @@ var options = {
 mongoose.Promise = global.Promise
 
 mongoose.connect('mongodb://localhost:27017/EmailSentimentAnalysisApp',options)
+
+mongoose.connection.on('error', () => {
+  console.log('Failed to connect to database shuting down')
+  process.exit(1)
+  fs.appendFileSync('./../error.log', 'Failed to connect to database on start up' )
+})
 
 module.exports = {mongoose}
